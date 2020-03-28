6d56916253608f056e4ffed1d1441a203a325341
DarkZrice <crizba00@gmail.com>
2020-03-27 18:14:19 -0600

    version 1

---
+++ Tabla Usuarios.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/materialize.min.css">
+    <script src="js/materialize.js"></script>
+    <script src="js/materialize.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
